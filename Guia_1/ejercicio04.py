dfbe1ab57b064e5e5b6fbc997f1a526fd82fc59a
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-09-14 15:21:24 -0300

    Update ejercicio04.py

--- Guia_1/ejercicio04.py
+++ Guia_1/ejercicio04.py
@@ -1,7 +1,5 @@
-"""División con resto. Plantear un algoritmo que 
-permita informar, para dos valores a y b el 
-resultado de la división a/b y el resto de esa 
-división."""
+"""División con resto. Plantear un algoritmo que permita informar, para dos valores a y b el 
+resultado de la división a/b y el resto de esa división."""
 
 #declaración de variables
 num1 = 0.0
@@ -9,11 +7,16 @@ num2 = 0.0
 division = 0.0
 resto = 0.0
 
+#mostramos por pantalla el titulo del ejercicio
+print("-- D I V I S I O N  C O N  R E S T O --")
+
+#preguntamos al usuario los dos numeros
 num1 = float(input("Ingrese un numero: "))
 num2 = float(input("Ingrese un segundo numero: "))
 
-#division y resto
+#hacemos la division y resto
 division = num1 / num2
 resto = num1 % num2
 
+#mostramos por pantalla lo pedido
 print("El resultado de la división es", division, ", y su resto es", resto)
