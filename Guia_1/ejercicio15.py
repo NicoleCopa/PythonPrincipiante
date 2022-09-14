16b1aadc394775678a32988c85f8779ee161be1f
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-09-14 15:24:22 -0300

    Update ejercicio15.py

--- Guia_1/ejercicio15.py
+++ Guia_1/ejercicio15.py
@@ -1,22 +1,28 @@
-"""Crear un conversor de pies y pulgadas 
-a centímetros."""
-
-#1 pie = 12 pulgadas
-#1 pulgada = 2.54 centimetros
-
+"""Crear un conversor de pies y pulgadas a centímetros."""
+#declaramos variables
 pies = 0.0
 pulgadas = 0.0
 centimetros = 0.0
 pregunta = 0
 
+#le preguntamos al usuario que desea convertir
 pregunta = int(input("¿Que desea convertir a centimetros?: 1-pies 2-pulgadas: "))
-centimetros = float(input("¿Cuantos centimetros desea convertir?: "))
 
+#usamos condicional para la opcion elegida
 if pregunta == 1:
-    pies = 30.48 * centimetros
-    print(centimetros, "cm equivalen a: ", pies, "pies.")
+    #pedimos al usuario la medida en pies
+    pies = float(input("Ingrese la medida en píes: "))
+    #calculamos los centimetros
+    centimetros = 30.48 * pies
+    #mostramos en pantalla el mensaje con la conversion hecha
+    print(pies, "pies equivalen a: ", centimetros, "centimetros.")
 elif pregunta == 2:
-    pulgadas = 2.54 * centimetros
-    print(centimetros, "cm equivalen a: ", pulgadas, "pulgadas.")
+    #pedimos al usuario la medida en pulgadas
+    pulgadas = float(input("Ingrese la medida en pulgadas: "))
+    #calculamos los centimetros
+    centimetros = 2.54 * pulgadas
+    #mostramos en pantalla el mensaje con la conversion hecha
+    print(pulgadas, "cm equivalen a: ", centimetros, "pulgadas.")
 else:
+    #mostramos mensaje de error en caso de no seleccional un numero valido
     print("Valor ingresado incorrecto")
