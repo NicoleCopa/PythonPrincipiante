15d110aef6447a9e80d5c25e8d61aa16fec38ad0
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-09-14 15:18:41 -0300

    Update ejercicio01.py

--- Guia_1/ejercicio01.py
+++ Guia_1/ejercicio01.py
@@ -5,15 +5,19 @@
 kilogramo = 0.0
 libra = 0.0
 
+#mostramos por pantalla el titulo del ejercicio
 print("-- P E S O  A  K I L O G R A M O --")
 print("")
 
 #ingreso de variable
 kilogramo = float(input("Ingrese un peso en kilogramos: "))
 
-#valildacion
+#valildacion si es mayor a 0
 if (kilogramo >= 0):
+    #sacamos el calculo de los kilos a libras
     libra = kilogramo * 2.2
+    #mostramos en pantalla lo pedido
     print("El peso ingresado de", kilogramo, "kg equivale a", libra, "lbrs.")
+#si es menor a 0, mostramos un mensaje de error
 else:
     print("El valor ingresado debe ser mayor o igual a 0.")
