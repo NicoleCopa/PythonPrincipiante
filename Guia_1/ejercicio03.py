a964e93cc9fda5d239d629b6c91b8969ff7b2492
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-09-14 15:20:25 -0300

    Update ejercicio03.py

--- Guia_1/ejercicio03.py
+++ Guia_1/ejercicio03.py
@@ -1,8 +1,11 @@
 """Escriba un programa que transforme todas las letras de una palabra
  en mayúsculas, minúsculas y la primera con minúsculas(capitalización)."""
 
-palabra = ""
+#declaramos variable
+palabra = str()
 
+#preguntamos al usuario una palabra
 palabra = str(input("Ingrese una palabra: "))
 
+#mostramos por pantalla la palabra como se pide usando capitalizacion, upper y lower 
 print(palabra.upper(), palabra.lower(), palabra.capitalize())
