b0acd11a38a61d271803ed0e7dc52102aa63177b
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-09-14 15:21:56 -0300

    Update ejercicio05.py

--- Guia_1/ejercicio05.py
+++ Guia_1/ejercicio05.py
@@ -1,22 +1,22 @@
-"""Cuadrado de un binomio. Plantear un script
-directamente en el shell de Python, que permita 
-mostrar, para dos valores 𝑎 y 𝑏, que: Un binomio al 
-cuadrado (suma) es igual al cuadrado del primer 
-término, más el doble producto del primero por el 
-segundo más el cuadrado del segundo."""
+"""Cuadrado de un binomio. Plantear un script directamente en el shell de Python, que permita 
+mostrar, para dos valores 𝑎 y 𝑏, que: Un binomio al cuadrado (suma) es igual al cuadrado del primer 
+término, más el doble producto del primero por el segundo más el cuadrado del segundo."""
 
-a = int(0)
-b = int(0)
-binomio = int(0)
+#declaramos las variables
+a = int()
+b = int()
+binomio = int()
 
-# (a+b)^2 = (a^2 + 2ab + b^2)
-
+#mostramos por pantalla el titulo del ejercicio
 print("-- B I N O M I O  A L  C U A D R A D O --")
 print("")
 
+#preguntamos al usuario el valor de a y b
 a = int(input("Ingrese el valor de 'a': "))
 b = int(input("Ingrese el valor de 'b': "))
 
+#hacemos el calculo de binomio
 binomio = ((a**2) + (2*a*b) + (b**2))
 
+#mostramos en pantalla el resultado pedidio
 print("El resultado del binomio al cuadrado es: " + str(binomio))
