2790c491c7d434de78b7f02da5abbc003d3edcd6
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-09-14 15:19:59 -0300

    Update ejercicio02.py

--- Guia_1/ejercicio02.py
+++ Guia_1/ejercicio02.py
@@ -1,11 +1,20 @@
 """Escribir un programa que pregunte un nombre de usuario, y que 
 después muestre por pantalla si su cantidad de letras es par o impar."""
 
-nombre = ""
+#declaramos variables
+nombre = str()
 nombre = str(input("Ingrese su nombre: "))
+
+#mostramos en pantalla el titulo del ejercicio
+print("-- N O M B R E S --")
+
+#usamos len para saber cuantas letras tiene el nombre y lo guardamos en contador
 contador = int(len(nombre)) % 2
 
+#usamos una condicional dependiendo si el nombre es par o impar
 if (contador == 0):
+    #mostramos mensaje si el nombre es par
     print("El nombre -", nombre, "- tiene una cantidad de lentras par (", int(len(nombre)), ")")
 else:
+    #mostramos mensaje si el nombre es impar
     print("El nombre -", nombre, "- tiene una cantidad de lentras impar (", int(len(nombre)), ")")
