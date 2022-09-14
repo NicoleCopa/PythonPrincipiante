2da78ffd3a5d8c07ed2979b036d946b2d677a75b
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-09-14 15:23:25 -0300

    Update ejercicio11.py

--- Guia_1/ejercicio11.py
+++ Guia_1/ejercicio11.py
@@ -1,11 +1,17 @@
-"""Multiplicación. Ingresar un número cualquiera
-por teclado y que muestre su respectiva tabla 
-del 1 al 10."""
+"""Multiplicación. Ingresar un número cualquiera por teclado y que muestre su respectiva tabla del 1 al 10."""
 
+#declaramos la variable
 num = 0
 
+#mostramos por pantalla el titulo del ejercicio
+print("-- M U L T I P L I C A C I O N --")
+
+#le preguntamos al usuario el numero
 num = int(input("Ingrese un numero: "))
 
+#usamos for apra ir de a 1 a 11
 for i in range (1,11):
+    #mostramos num, i y num*i
     print(num, "x", i, "=", num*i)
+    #le multiplicamos num a i y lo declaramos en i
     i = i*num
