b9e155ee95a85d7a5a0f61534295b5ae283e8a82
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-09-14 15:23:13 -0300

    Update ejercicio10.py

--- Guia_1/ejercicio10.py
+++ Guia_1/ejercicio10.py
@@ -1,14 +1,20 @@
-"""Ingresar la cantidad de números de la 
-sucesión de Fibonacci a mostrar."""
+"""Ingresar la cantidad de números de la sucesión de Fibonacci a mostrar."""
 
+#declaramos las variables
 a = 0
 b = 1
 c = 0
 
+#mostramos por pantalla un mensaje de lo que vamos a mostrar
 print("Los numeros de la sucesión de Fibonacci son: ")
 
+#usamos for para ir de 1 a 10 con paso 1
 for i in range (1,10,1):
+    #mostramos la variable c
     print(c)
+    #igualamos a con b
     a = b
+    #igualamos b con c
     b = c
+    #sumamos a + b y lo igualamos con c
     c = a+b
