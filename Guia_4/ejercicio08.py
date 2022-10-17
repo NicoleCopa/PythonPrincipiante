41a57222a911f148d3c0b55e859a0cd807d6932e
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-10-17 15:47:52 -0300

    Add files via upload

---
+++ Guia_4/ejercicio08.py
@@ -0,0 +1,4 @@
+"""Queremos guardar los nombres y las edades de los alumnos de un curso. 
+Realiza un programa que introduzca el nombre y la edad de cada alumno. 
+El proceso de lectura de datos terminará cuando se introduzca como nombre un asterisco (*) 
+Al finalizar se mostrará los siguientes datos: Todos los alumnos mayores de edad y los alumnos mayores (los que tienen más edad)."""
