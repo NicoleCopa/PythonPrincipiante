b6998e04bc87b40b5e0ef8084e39b21968d0f409
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-11-01 16:45:22 -0300

    Update ejercicio05.py

--- Guia_4/ejercicio05.py
+++ Guia_4/ejercicio05.py
@@ -1,2 +1,19 @@
 """Hacer un programa que inicialice una lista de números con valores aleatorios (10 valores), 
 y posterior ordene los elementos de menor a mayor."""
+
+import random
+#declaramos variables y lista
+lista = []
+numeros = int()
+ordenados = int()
+#usamos for para que se repita 10 veces el programa
+for i in range (10):
+    #declaramos un numero aleatorio en numeros
+    numeros = random.randrange(10)
+    #agregamos el valor a la lista
+    lista.append(numeros)
+#declaramos ordenados como la lista ordenada de forma ascendente con sorted
+ordenados = sorted(lista)
+#mostramos por pantalla lo pedido
+print("Los números inicializados de forma aleatória son:", lista)
+print("Los números ordenados de forma ascendente son:", ordenados)
