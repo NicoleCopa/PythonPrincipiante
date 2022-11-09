58272ce35ce010d83ae5ed6bb22facc3f53cbf22
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-11-09 20:47:47 -0300

    Update ejercicio08.py

--- Guia_4/ejercicio08.py
+++ Guia_4/ejercicio08.py
@@ -2,3 +2,22 @@
 Realiza un programa que introduzca el nombre y la edad de cada alumno. 
 El proceso de lectura de datos terminará cuando se introduzca como nombre un asterisco (*) 
 Al finalizar se mostrará los siguientes datos: Todos los alumnos mayores de edad y los alumnos mayores (los que tienen más edad)."""
+
+listaNombres = []
+alumnoMayor = []
+ingresoNombre = str()
+ingresoEdades = int()
+maximo = int()
+
+while ingresoNombre != "*":
+    ingresoNombre = str(input("Ingrese el nombre del alumno - para finalizar ingrese '*': "))
+    if ingresoNombre != "*":
+        ingresoEdades = int(input("Ingrese la edad del alumno: "))
+    if ingresoEdades >= 18:
+        listaNombres.append(ingresoNombre)
+    if maximo < ingresoEdades:
+        alumnoMayor.append(ingresoNombre)
+        maximo = ingresoEdades
+
+print("Los alumnos mayores de edad son:", listaNombres)
+print("Los alumnos mayores son:", alumnoMayor)
