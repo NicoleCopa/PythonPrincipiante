40e1a1226250f3d923633614d7b24b7d940df33c
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-11-11 17:01:55 -0300

    Update ejercicio07.py

--- Guia_4/ejercicio07.py
+++ Guia_4/ejercicio07.py
@@ -2,19 +2,26 @@
 enteros cada uno, pida valores para ‘lista1’ y ‘lista2’ y calcule 
 lista3 = lista1 + lista2."""
 
-import random
-
+#declaramos listas
 lista1 = []
 lista2 = []
 lista3 = []
 
+#usamos for para que el programa se repita 5 veces
 for i in range (0,5):
-    numero1 = random.randrange(100)
-    numero2 = random.randrange(100)
+    #pedimos al usuario un numero la lista1
+    numero1 = int(input("Ingrese un numero para la lista 1: "))
+    #agregamos el numero a lista1
     lista1.append(numero1)
+    #pedimos al usuario un numero la lista2
+    numero2 = int(input("Ingrese un numero para la lista 2: "))
+    #agregamos el numero a lista2
     lista2.append(numero2)
+    #sumamos el valor de lista1 y lista2 y lo agregamos a lista3
     lista3.append(sum(lista1+lista2))
+    #sumamos i
     i = i + 1
-
-print(lista1,lista2)
-print(lista3)
+#mostramos por pantalla lo pedido
+print("Lista 1:", lista1)
+print("Lista 2:", lista2)
+print("La suma de ambas listas:", lista3)
