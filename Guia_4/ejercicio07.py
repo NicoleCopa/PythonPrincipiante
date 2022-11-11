7dd2914a41b9e07282f882480ce40539519c0c77
Nicole Copa <67641333+NicoleCopa@users.noreply.github.com>
2022-11-11 17:02:04 -0300

    Update ejercicio07.py

--- Guia_4/ejercicio07.py
+++ Guia_4/ejercicio07.py
@@ -1,6 +1,5 @@
 """Programa que declare tres listas ‘lista1’, ‘lista2’ y ‘lista3’ de cinco 
-enteros cada uno, pida valores para ‘lista1’ y ‘lista2’ y calcule 
-lista3 = lista1 + lista2."""
+enteros cada uno, pida valores para ‘lista1’ y ‘lista2’ y calcule lista3 = lista1 + lista2."""
 
 #declaramos listas
 lista1 = []
